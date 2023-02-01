80e1a2d0e8c06494e6a244d199fdfe9bc58530ae
kittty0520 <dheud1004@gmail.com>
2023-02-01 18:23:27 +0900

    Service Using Context APIs

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -4,14 +4,16 @@ import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
 import FakeYoutube from '../api/fakeYoutube';
 import Youtube from '../api/youtube';
+import { useYoutubeApi } from '../context/YoutubeApiContext';
+
 export default function Videos() {
 	const { keyword } = useParams();
+	const { youtube } = useYoutubeApi();
 	const {
 		isLoading,
 		error,
 		data: videos,
 	} = useQuery(['videos', keyword], () => {
-		const youtube = new Youtube();
 		return youtube.search(keyword);
 	});
 
