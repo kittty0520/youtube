192497ef325e3dc19ed46d608607571780fef57f
kittty0520 <dheud1004@gmail.com>
2023-02-01 17:58:49 +0900

    Real Service Using API

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
 import FakeYoutube from '../api/fakeYoutube';
+import Youtube from '../api/youtube';
 export default function Videos() {
 	const { keyword } = useParams();
 	const {
@@ -10,7 +11,7 @@ export default function Videos() {
 		error,
 		data: videos,
 	} = useQuery(['videos', keyword], () => {
-		const youtube = new FakeYoutube();
+		const youtube = new Youtube();
 		return youtube.search(keyword);
 	});
 
